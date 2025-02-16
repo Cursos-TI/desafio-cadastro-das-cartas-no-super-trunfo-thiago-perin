e7e227529b239439bf545354679e4c51ff841926
thiago-perin <thipinta@gmail.com>
2025-02-16 23:39:58 +0000

    melhoras

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,8 +5,8 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    int pontos, populacao;
-    float pib, area;
+    int pontos;
+    float populacao, pib, area;
     char nome[3];
 
 		// nome do aplicativo
@@ -17,16 +17,17 @@ int main() {
 		scanf("%s", &nome);
 
  	   // Colocando o valor da população
-		printf("Digite o tamanho da população da cidade: \n");
-		scanf("%d", &populacao);
+		printf("Digite o tamanho da população da cidade em milhares de habitantes: \n");
+		scanf("%f", &populacao);
 
    	 // Colocando a área da cidade
 		printf("Digite o valor da área da cidade em quilômetros quadrados: \n");
 		scanf("%f", &area);
     	
  	   // Colocando o pib da cidade
-  	  printf("Digite o pib da cidade: \n");
-		scanf("%f", &pib);
+  	  printf("Digite o pib da cidade em milhões de reais: \n");
+
+	scanf("%f", &pib);
     
      	   // Colocando o pib da cidade
   	  printf("Digite quantos pontos turisticos tem na cidade: \n");
@@ -34,9 +35,9 @@ int main() {
 		
     printf("Cidade cadastrada: \n");
 	printf("Nome da cidade: %s \n", nome);
-	printf("População da cidade: %d \n", populacao);
+	printf("População da cidade: %d mil habitantes \n", populacao);
 	printf("Area da cidade: %.2f Kmˆ2 \n", area);
-	printf("PIB da cidade: %.2f \n", pib);
+	printf("PIB da cidade: %.2f milhões de reais\n", pib);
 	printf("Quantidade de pontos turisticos: %d \n", pontos);
     
     // Exibição dos Dados das Cartas:
